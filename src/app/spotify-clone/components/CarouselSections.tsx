6f6e2ff595b58fc23496f286a0cccd06e800a6bd
lucasalc25 <lucas.alc25@gmail.com>
2025-07-30 22:10:45 -0400

    v2.0

--- src/app/spotify-clone/components/CarouselSections.tsx
+++ src/app/spotify-clone/components/CarouselSections.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { ChevronLeft, ChevronRight, Play } from "lucide-react";
 
 interface Slide {
@@ -34,6 +34,29 @@ export default function CarouselSections({
 
   const handleNext = () => {
     if (canGoNext) setStartIndex((prev) => prev + 1);
+  };
+
+  {
+    /* TOUCH */
+  }
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    const deltaX = touchEndX.current - touchStartX.current;
+    if (deltaX > 50 && canGoPrev) {
+      handlePrev(); // swipe para a direita
+    } else if (deltaX < -50 && canGoNext) {
+      handleNext(); // swipe para a esquerda
+    }
   };
 
   return (
@@ -78,49 +101,95 @@ export default function CarouselSections({
         </button>
 
         {/* Slides */}
-        <div className="overflow-hidden relative">
-          <div
-            className="grid grid-flow-col -ml-2 overflow-x z-10 transition-transform duration-500 ease-in-out"
-            style={{ transform: `translateX(${offset}px)` }}
-          >
-            {slides.map((slide, index) => (
-              <div
-                key={`${keyPrefix}-${index}`}
-                className="flex flex-col items-center text-center w-[44.8vw] h-[33vh] max-w-[195px] min-w-[168px] max-h-[295px] min-h-[220px] shrink-0 overflow-hidden"
-              >
-                {/* Aqui é o novo group */}
-                <div className="group inline-flex flex-col gap-2 p-2 rounded-md hover:bg-[#1F1F1F] relative cursor-pointer">
-                  <div className="relative w-[40.53vw] max-w-[171px] h-[22.8vh] max-h-[171px] rounded-xl shadow">
-                    <img
-                      src={slide.src}
-                      alt={slide.title}
-                      className={`absolute w-full h-full left-0 right-0 ${
-                        slide.src.includes("/artists/")
-                          ? "rounded-full max-sm:rounded-md"
-                          : "rounded-md"
-                      }`}
-                    />
-                    <button className="opacity-0 pointer-events-none absolute z-20 top-35 left-35 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-[#35D865] cursor-pointer rounded-full transition-opacity duration-300 filter hover:scale-[1.05] hover:brightness-105 group-hover:opacity-100 group-hover:pointer-events-auto">
-                      <Play className="fill-black text-black" />
-                    </button>
-                  </div>
+        <div className="relative">
+          {/* Desktop */}
+          <div className="overflow-hidden relative">
+            <div
+              className="hidden sm:grid grid-flow-col -ml-3 overflow-x z-10 transition-transform duration-500 ease-in-out"
+              onTouchStart={handleTouchStart}
+              onTouchMove={handleTouchMove}
+              onTouchEnd={handleTouchEnd}
+              style={{ transform: `translateX(${offset}px)` }}
+            >
+              {slides.map((slide, index) => (
+                <div
+                  key={`${keyPrefix}-${index}`}
+                  className="flex flex-col items-center text-center w-[44.8vw] h-[33vh] max-w-[195px] min-w-[168px] max-h-[295px] min-h-[220px] shrink-0 overflow-hidden"
+                >
+                  {/* Aqui é o novo group */}
+                  <div className="group inline-flex flex-col gap-2 p-2 rounded-md hover:bg-[#1F1F1F] relative cursor-pointer">
+                    <div className="relative w-[40.53vw] max-w-[171px] h-[22.8vh] max-h-[171px] rounded-xl shadow">
+                      <img
+                        src={slide.src}
+                        alt={slide.title}
+                        className={`absolute w-full h-full left-0 right-0 ${
+                          slide.src.includes("/artists/")
+                            ? "rounded-full max-sm:rounded-md"
+                            : "rounded-md"
+                        }`}
+                      />
+                      <button className="opacity-0 pointer-events-none absolute z-20 top-35 left-35 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-[#35D865] cursor-pointer rounded-full transition-opacity duration-300 filter hover:scale-[1.05] hover:brightness-105 group-hover:opacity-100 group-hover:pointer-events-auto">
+                        <Play className="fill-black text-black" />
+                      </button>
+                    </div>
 
-                  <div className="flex flex-col items-start">
-                    <h3 className="text-base font-semibold">{slide.title}</h3>
-                    <p
-                      className={`text-sm text-[#B3B3B3] text-start line-clamp-2 ${
-                        slide.title === ""
-                          ? " max-sm:text-white max-sm:text-base max-sm:font-normal"
-                          : "max-sm:hidden"
-                      }`}
-                    >
-                      {slide.description}
-                    </p>
+                    <div className="flex flex-col items-start">
+                      <h3 className="text-base font-semibold">{slide.title}</h3>
+                      <p
+                        className={`text-sm text-[#B3B3B3] text-start line-clamp-2 ${
+                          slide.title === ""
+                            ? " max-sm:text-white max-sm:text-base max-sm:font-normal"
+                            : "max-sm:hidden"
+                        }`}
+                      >
+                        {slide.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
+        </div>
+        {/* Mobile: carrossel com scroll touch */}
+        <div className="sm:hidden overflow-x-auto flex gap-2 snap-x snap-mandatory scrollbar-hidden px-2 -ml-3">
+          {slides.map((slide, index) => (
+            <div
+              key={`${keyPrefix}-${index}`}
+              className="flex flex-col items-center text-center w-[44.8vw] h-[33vh] max-w-[195px] min-w-[168px] max-h-[295px] min-h-[220px] shrink-0 overflow-hidden"
+            >
+              {/* Aqui é o novo group */}
+              <div className="group inline-flex flex-col gap-2 p-2 rounded-md hover:bg-[#1F1F1F] relative cursor-pointer">
+                <div className="relative w-[40.53vw] max-w-[171px] h-[22.8vh] max-h-[171px] rounded-xl shadow">
+                  <img
+                    src={slide.src}
+                    alt={slide.title}
+                    className={`absolute w-full h-full left-0 right-0 ${
+                      slide.src.includes("/artists/")
+                        ? "rounded-full max-sm:rounded-md"
+                        : "rounded-md"
+                    }`}
+                  />
+                  <button className="opacity-0 pointer-events-none absolute z-20 top-35 left-35 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-[#35D865] cursor-pointer rounded-full transition-opacity duration-300 filter hover:scale-[1.05] hover:brightness-105 group-hover:opacity-100 group-hover:pointer-events-auto">
+                    <Play className="fill-black text-black" />
+                  </button>
+                </div>
+
+                <div className="flex flex-col items-start">
+                  <h3 className="text-base font-semibold">{slide.title}</h3>
+                  <p
+                    className={`text-sm text-[#B3B3B3] text-start line-clamp-2 ${
+                      slide.title === ""
+                        ? " max-sm:text-white max-sm:text-base max-sm:font-normal"
+                        : "max-sm:hidden"
+                    }`}
+                  >
+                    {slide.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
